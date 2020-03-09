6ce375ee97c401fbb9639450bef3713a4c4469ec
Andrew <awhsg11@gmail.com>
2020-03-09 21:36:32 +0000

    User-Can-View-The-Products-For-Sale-Back-End

--- server/index.js
+++ server/index.js
@@ -14,8 +14,16 @@ app.use(sessionMiddleware);
 app.use(express.json());
 
 app.get('/api/health-check', (req, res, next) => {
-  db.query('select \'successfully connected\' as "message"')
-    .then(result => res.json(result.rows[0]))
+  const sql = `
+      select "productId",
+             "name",
+             "price",
+             "image",
+             "shortDescription"
+        from "products"
+  `;
+  db.query(sql)
+    .then(result => res.json(result.rows))
     .catch(err => next(err));
 });
 

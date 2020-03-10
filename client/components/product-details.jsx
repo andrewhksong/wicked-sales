c3050baa23102b97f2d58f0cc0108f62f4680121
Andrew <awhsg11@gmail.com>
2020-03-10 20:02:32 +0000

    Added ProductDetails Component

---
+++ client/components/product-details.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class ProductDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: null
+    };
+  }
+
+  render() {
+    return null;
+  }
+}

57bc8ee98343553f85d3a0259de8d363857c11f9
Andrew <awhsg11@gmail.com>
2020-03-10 20:25:16 +0000

    Passed App Component's setView Method through ProductList via Props

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -24,7 +24,7 @@ export default class App extends React.Component {
     return (
       <div className="container">
         <Header />
-        <ProductList />
+        <ProductList onClick={ this.setView }/>
       </div>
     );
   }

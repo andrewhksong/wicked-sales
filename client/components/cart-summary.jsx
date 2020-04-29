dcc4ce048cd2430852e17f6ec8523a195bde99c7
andrewhksong <andrewhksong@gmail.com>
2020-04-29 02:02:30 -0700

    Added Hover Effects

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -22,13 +22,13 @@ export default class CartSummary extends React.Component {
     }
     return (
       <div className="container mb-5">
-        <p className="text-muted"
+        <p className="redirect text-muted"
            style={{ cursor: 'pointer' }}
            onClick={() => this.props.setView('catalog', {})}>
            &lt; Back to catalog
         </p>
         <h2 className="mb-2">My Cart</h2>
-        <div className="card p-2 m-1">
+        <div className="cart p-2 m-1">
           { cartList }
           <div className="d-flex justify-content-between p-3">
             <h3 className="m-1">Cart Total ${cartTotal}</h3>
